4c2811e9483f8ec9a416ad3423fc7eff38407f46
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-18 22:30:08 -0500

    Two-inpur multiplexer definition

---
+++ rtl/mux.v
@@ -0,0 +1,26 @@
+
+
+
+module mux (
+  sel_in,
+  data_a_in,
+  data_b_in,
+  data_out
+);
+
+// Parameter definition
+parameter DATA_WIDTH = 4;
+
+// Input and output definition
+input sel_in;
+input [DATA_WIDTH-1:0] data_a_in;
+input [DATA_WIDTH-1:0] data_b_in;
+output reg [DATA_WIDTH-1:0] data_out;
+
+// Combinational logic
+always @ ( * ) begin
+  if (sel_in)
+    data_out = data_a_in;
+  else
+    data_out = data_b_in;
+end
