7c63360f2f8ef3ec66ebb8af8f4b4a77f42a41e9
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-19 00:20:05 -0500

    Simulation outputs to file

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,3 +1,6 @@
+
+#include <fstream>
+#include <iostream>
 #include "verilated.h"
 #include "Vfibonacci_top.h"
 
@@ -7,21 +10,51 @@ int main(int argc, char const *argv[]) {
   // Starts verilator
   Verilated::commandArgs(argc, argv);
 
+  // Creates the output file
+  FILE *output_file;
+  output_file = fopen("output_data.l0gic", "w");
+
+  // Prints simulation
+  std::fprintf(output_file, "CLOCK_CYCLE, LED_OUT", );
+
   // Creates the state machine object
   Vfibonacci_top* state = new Vfibonacci_top;
 
   // Creates a constant to set the clock cycle maximum
   const int NCLOCKS = 100000;
 
-  // Runs the simulation for the given clock cycles
-  for (int cc = 0; cc < NCLOCKS; cc++){
+  // Variable to store the led output
+  int led_val;
 
-    core
+  // Runs the simulation for the given clock cycles
+  for (int cc = 0; cc < NCLOCKS*2; cc++){
+
+    // Assigns the clock cycle value
+    if(cc % 2 == 0) {
+      state->clock_in = 1;
+    } else {
+      state->clock_in = 0;
+    }
+
+    // Assigns the reset signal
+    if (cc < 10 * 2){
+      state->reset_in = 1;
+    } else {
+      state->reset_in = 0;
+    }
 
     // Evaluates the state machine
     state->eval();
 
+    // Gets the led value
+    led_val = state->led_out;
+
+    // Prints the sim out
+    std::cout << led_val << std::endl;
+
   }
+
+  fclose(output_file);
 
   exit(EXIT_SUCCESS);
 }
