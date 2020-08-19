1b0f76ac14f98e9b46d37d74c39405c633f2d79a
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-18 21:25:50 -0500

    General purpose register description

---
+++ rtl/gen_reg.v
@@ -0,0 +1,46 @@
+
+
+
+module gen_reg (
+  clock_in,
+  reset_in,
+  set_in,
+  data_in,
+  data_out
+);
+
+// Parameter definition
+parameter DATA_WIDTH = 4;
+
+// Input and output definition
+input clock_in;
+input reset_in;
+input set_in;
+input [DATA_WIDTH-1:0] data_in;
+output [DATA_WIDTH-1:0] data_out;
+
+// Register definition
+reg [DATA_WIDTH-1:0] data_hold;
+reg [DATA_WIDTH-1:0] data_store;
+
+// Combinational logic
+always @ ( * ) begin
+  if(set_in)
+    data_hold = data_in;
+  else
+    data_hold = data_hold;
+end
+
+// Sequential logic
+always @ ( posedge clock_in, posedge reset_in ) begin
+  if (reset_in == 1'b1)
+    data_store = 0;
+  else
+    data_store = data_hold;
+end
+
+// Ouput assignment
+assign data_out = data_store;
+
+
+endmodule
