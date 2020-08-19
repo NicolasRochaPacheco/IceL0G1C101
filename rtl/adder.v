ec0edf0649e7136e539379b84fe9e5d30f7b41b4
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-18 22:00:36 -0500

    Adder module intial definition

---
+++ rtl/adder.v
@@ -0,0 +1,20 @@
+
+
+module adder (
+  data_a_in,
+  data_b_in,
+  data_out
+);
+
+// Parameter definition
+parameter DATA_WIDTH = 4;
+
+// Input and output definition
+input [DATA_WIDTH-1:0] data_a_in;
+
+// Combinational logic
+always @ ( * ) begin
+
+end
+
+endmodule
