3273e1ef0430de85f3e51362003f2a01af5ed3f9
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-18 22:28:49 -0500

    Full adder description

--- rtl/adder.v
+++ rtl/adder.v
@@ -11,10 +11,12 @@ parameter DATA_WIDTH = 4;
 
 // Input and output definition
 input [DATA_WIDTH-1:0] data_a_in;
+input [DATA_WIDTH-1:0] data_b_in;
+output reg [DATA_WIDTH-1:0] data_out;
 
 // Combinational logic
 always @ ( * ) begin
-
+  data_out = data_a_in + data_b_in;
 end
 
 endmodule
