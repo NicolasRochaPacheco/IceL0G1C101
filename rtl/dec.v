5c895449e33f5bdf5d1287f9de367316f64d3c91
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-08-18 22:29:51 -0500

    Decrement module description

---
+++ rtl/dec.v
@@ -0,0 +1,20 @@
+
+
+module dec (
+  data_in,
+  data_out
+);
+
+// Parameter definition
+parameter DATA_WIDTH = 4,
+
+// Input and output definition
+input [DATA_WIDTH-1:0] data_in;
+output reg [DATA_WIDTH-1:0] data_out;
+
+// Combinational logic
+always @ ( * ) begin
+  data_out = data_in - 1;
+end
+
+endmodule
